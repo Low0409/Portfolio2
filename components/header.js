3bf272de6d9884c9a323d37a8ca50c46f1583a18
Low0409 <kitagawa0409@icloud.com>
2022-01-08 14:39:12 +0900

    build

--- components/header.js
+++ components/header.js
@@ -5,13 +5,16 @@ function Header() {
   return (
     <header className="py-6">
       <Container>
-        <nav className="flex space-x-4">
+        <nav className="flex space-x-5">
           <Link href="/">
             <a>Top</a>
           </Link>
 
             <a href='//relife-campus.com'>WordPress</a>
           <a href='//github.com/Low0409'>GitHub</a>
+          <Link href="/Skill">
+            <a>触ったことがある技術</a>
+          </Link>
         </nav>
       </Container>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -29,27 +29,27 @@ function HomePage() {
             />
           </div>
           <p>多機能チャットアプリ。ログイン機能、チャンネルが分かれており、自分でチャンネルを作成することもできます。</p>
-          <p>またスタンプも送信出来たり,Twitterでいうところのファボ💓機能に加えて</p>
-          <p>上の画像にあるように画像ファイルを送れたり,ファイルの共有ができます。(多くの拡張子に対応しています)</p>
+          <p>またスタンプも送信出来たり,Twitterでいうところのファボ💓機能に加えて、画像を送れたり</p>
+          <p>ファイルの共有ができます。(txt,pdfなど多くの拡張子に対応しています)</p>
           <p>【使用した主な技術】:Express/Socket.io/FirebaseRealTimeDB・Storage/JQuery/bootstrap</p>
 
         <h2 className="text-2xl font-bold mt-8 bg-gray-500 shadow-sm text-white p-3">・ビデオチャットWebアプリ</h2>
-        <p>Sky Way apiを使用したビデオチャットアプリ。</p>
-        <p>Nimbleの機能として付け加えた</p>
+        <p>SkyWayAPIを使用したビデオチャットアプリです。</p>
+        <p>上のチャットアプリNimbleのビデオチャット機能として付け加えました。</p>
         <p>【使用した主な技術】:SkyWayAPI</p>
 
 
         <h2 className="text-2xl font-bold mt-8 bg-gray-500 shadow-sm text-white p-3">・グラデーションジェネレーター</h2>
-        <a href="https://gradation-app.vercel.app/">デプロイしてます　https://gradation-app.vercel.app/</a>
+        <a href="https://gradation-app.vercel.app/">デプロイしてます https://gradation-app.vercel.app/</a>
         <Image
               src="/gradation.PNG"
               alt="Nimble"
               width={1920 / 2}
               height={1200 / 2}
             />
-            <p>よくあるグラデーションWebアプリは多機能だけど色を自分で決めて比率を自分で調整するなどが不便で</p>
-            <p>面倒なところがあるのでシンプルなものを作ってみたい思った。Next.jsで初めて作ったまともなWebアプリ</p>
-            <p>カラー変更ボタンを押す度ランダムに色が変わる。ボタンを押し続けて直感でいいなと思った色を見つけられる。</p>
+            <p>よくあるグラデーションWebアプリは多機能だけど色を自分で決めて比率を自分で調整するなどが不便です。</p>
+            <p>カラー変更ボタンを押す度ランダムに色が変わり、直感でいいなと思った色を見つけられます。</p>
+            <p>気が向いたら3色に切り替えられるようにしたいと思います。</p>
             <p>【使用した主な技術】:Next.js</p>
 
 
