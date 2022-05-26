3d8ea45599dd41fbc282a4bbec070dda22bd0245
Low0409 <kitagawa0409@icloud.com>
2022-05-26 22:45:38 +0900

    0526

--- pages/index.js
+++ pages/index.js
@@ -78,16 +78,16 @@ function HomePage() {
           <p>表示するコンテンツの選定→AdobeXDでサンプル作成→コーディングのステップを踏んで作成しました。</p>
           <p>9割がた画像のデザインも一人で作成したのですが、Adobe系ツールをガッツリ触れることができたので良かったです</p>
 
-          <h2 className="text-2xl font-bold mt-8 bg-gray-500 shadow-sm text-white p-3">Instagram</h2>
+          <h2 className="text-2xl font-bold mt-8 bg-gray-500 shadow-sm text-white p-3">Instagram Graph API</h2>
           <Image
-            src="/insta3.jpg"
-            alt="FC"
+            src="/ins.JPG"
+            alt="Instagram"
             width={1200}
             height={600}
           />
-          <p>アルバイトでの最初の業務にてInstagram Graph APIでの投稿表示を作成しました。</p>
-          <p>今まではSnapWidgetという有料サービスで投稿を表示していましたが、</p>
-          <p>各店舗ページを合わせると70以上あることもあり、コストがかかるということで、APIでの表示を実装しました。</p>
+          <p>アルバイトでの業務にてInstagram Graph APIでの投稿表示を作成しました。</p>
+          <p>これまではSnapWidgetという有料サービスを利用して投稿を表示していましたが、</p>
+          <p>各店舗ページを合わせると70以上あることもあり、各ページに導入するとなると、コストがかかるということで、APIでの表示を実装しました。</p>
         </div>
       </Container>
     </>
