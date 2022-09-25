a844dae4fd9d92fd19f55ba523461ac36afc7ea5
Low0409 <kitagawa0409@icloud.com>
2022-09-26 03:45:44 +0900

    refactor: style調整とproduct追加

--- components/Product.js
+++ components/Product.js
@@ -3,11 +3,9 @@ import Image from "next/dist/client/image";
 function Product(props) {
     const { name, img, width, height, about, time, people, role, text, tech } = props;
     return (
-        <div>
-
-            <h2 className="text-2xl font-bold bg-gray-500 shadow-sm text-white p-3">・{name}</h2>
+        <div className="bg-white rounded-lg shadow-2xl pb-8 border-2 border-gray-600">
+            <h2 className="text-2xl font-bold text-center bg-black text-white py-2 mt-4">{name}</h2>
             <div className="container max-w-4xl m-auto px-4 mt-10">
-
                 <Image
                     src={img}
                     alt={name}
@@ -15,13 +13,14 @@ function Product(props) {
                     height={height}
                 />
             </div>
-            <p>【概要】{about}</p>
-            <p>【開発期間】{time}</p>
-            <p>【開発人数】{people}人</p>
-            <p>【役割】{role}</p>
-            <p>【詳細・開発秘話】{text}</p>
-            <p>【使用した主な技術】:{tech}</p>
-
+            <div className="p-8">
+                <p>【概要】{about}</p>
+                <p>【開発期間】{time}</p>
+                <p>【開発人数】{people}人</p>
+                <p>【役割】{role}</p>
+                <p>【詳細・開発秘話】{text}</p>
+                <p>【使用した主な技術】:{tech}</p>
+            </div>
         </div>
     );
 }
--- components/header.js
+++ components/header.js
@@ -7,13 +7,13 @@ function Header() {
       <Container>
         <nav className="flex space-x-5">
           <Link href="/">
-            <a className='hover:bg-gray-200 p-1'>Top</a>
+            <a className='hover:text-gray-300'>Home</a>
           </Link>
 
-            <a className='hover:bg-gray-200  p-1' href='//relife-campus.com'>技術ブログ</a>
-          <a className='hover:bg-gray-200  p-1' href='//github.com/Low0409'>GitHub</a>
+            <a className='hover:text-gray-300  p-1' href='//relife-campus.com'>技術Blog</a>
+          <a className='hover:text-gray-300  p-1' href='//github.com/Low0409'>GitHub</a>
           <Link href="/Skill">
-            <a className='hover:bg-gray-200  p-1'>触れたことがある技術</a>
+            <a className='hover:text-gray-300  p-1'>Skills</a>
           </Link>
         </nav>
       </Container>
